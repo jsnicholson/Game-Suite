1bba7c5489c3df790841588cd57f56c27f992fb3
Josh Nicholson <jsnicholson08@gmail.com>
2018-11-28 00:12:35 +0000

    Added Tetrimino prefabs

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+    public Sprite tileSprite;
+
+    Tetromino[] tetrominoes;
+
+    void Start() {
+        Tetromino go = new Tetromino();
+    }
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour {
+    void Rotate() {
+
+    }
+
+    void Fall() {
+
+    }
+}
