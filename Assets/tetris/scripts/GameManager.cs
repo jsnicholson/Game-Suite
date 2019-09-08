873c304c03cd8f60f7b038c022a5838ea1a62227
Josh Nicholson <jsnicholson08@gmail.com>
2019-09-08 22:02:32 +0100

    Audio plays on line clear

--- Assets/tetris/scripts/GameManager.cs
+++ Assets/tetris/scripts/GameManager.cs
@@ -11,13 +11,26 @@ public class GameManager : MonoBehaviour {
     // audio clips
     public AudioClip audioLineClear;
 
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void PieceLanded() {
         spawner.SpawnTetromino();
         ClearLines();
     }
 
     private void ClearLines() {
-        audioSource.clip = audioLineClear;
-        audioSource.Play();
+        bool b_lineFull = true;
+        for (int i = 0; i < grid.GetDimensions().x; i++) {
+            if(grid.GetGridAt(new Vector2(i, 0)) == null)
+                b_lineFull = false;
+            
+        }
+
+        if(b_lineFull) {
+            audioSource.clip = audioLineClear;
+            audioSource.Play();
+        }     
     }
 }
