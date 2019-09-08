66de790f6491ffe5a3daf7d1e7a21804e33b6dc3
Josh Nicholson <jsnicholson08@gmail.com>
2019-09-08 21:51:12 +0100

    Added line clear sound effect

--- Assets/tetris/scripts/GameManager.cs
+++ Assets/tetris/scripts/GameManager.cs
@@ -7,12 +7,17 @@ public class GameManager : MonoBehaviour {
     public GameGrid grid;
     public Spawner spawner;
 
+    private AudioSource audioSource;
+    // audio clips
+    public AudioClip audioLineClear;
+
     public void PieceLanded() {
         spawner.SpawnTetromino();
         ClearLines();
     }
 
     private void ClearLines() {
-
+        audioSource.clip = audioLineClear;
+        audioSource.Play();
     }
 }
