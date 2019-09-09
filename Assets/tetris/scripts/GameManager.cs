823b607f999f8d6d8a0d0aade41de2641cfbc3ff
Josh Nicholson <jsnicholson08@gmail.com>
2019-09-09 22:51:24 +0100

    Line being cleared when full
    still not moving other minos down

--- Assets/tetris/scripts/GameManager.cs
+++ Assets/tetris/scripts/GameManager.cs
@@ -7,30 +7,58 @@ public class GameManager : MonoBehaviour {
     public GameGrid grid;
     public Spawner spawner;
 
-    private AudioSource audioSource;
+    private AudioSource audio_source;
     // audio clips
-    public AudioClip audioLineClear;
+    public AudioClip audio_lineClear;
 
     void Start() {
-        audioSource = GetComponent<AudioSource>();
+        audio_source = GetComponent<AudioSource>();
     }
 
     public void PieceLanded() {
-        spawner.SpawnTetromino();
         ClearLines();
+        spawner.SpawnTetromino(); 
     }
 
+    /// <summary>
+    /// if the any lines are full then remove them
+    /// </summary>
     private void ClearLines() {
-        bool b_lineFull = true;
-        for (int i = 0; i < grid.GetDimensions().x; i++) {
-            if(grid.GetGridAt(new Vector2(i, 0)) == null)
-                b_lineFull = false;
-            
+        // list of line to clear
+        List<int> lint_linesToClear = new List<int>();
+        bool b_anyCleared = false;
+
+        // loop over grid
+        for (int j = 0; j < grid.GetDimensions().y; j++) {
+            // assume line is full
+            bool b_lineFull = true;
+            for(int i = 0; i < grid.GetDimensions().x; i++) {
+                // if grid at x,y is null, then line is not full
+                if (grid.GetGridAt(new Vector2(i, j)) == null)
+                    b_lineFull = false;   
+            }
+
+            // if line was full
+            if (b_lineFull) {
+                Debug.Log("Line full");
+                // add line to list to be cleared
+                lint_linesToClear.Add(j);
+                // flag
+                b_anyCleared = true;
+            }
         }
 
-        if(b_lineFull) {
-            audioSource.clip = audioLineClear;
-            audioSource.Play();
+        if(b_anyCleared) {
+            // remove bottom line
+            foreach(int line in lint_linesToClear) {
+                for(int i = 0; i < grid.GetDimensions().x; i++) {
+                    Destroy(grid.GetGridAt(new Vector2(i, line)));
+                    grid.SetGridAt(new Vector2(i, line), null);
+                }
+            }
+            // move all other minos down
+            audio_source.clip = audio_lineClear;
+            audio_source.Play();
         }     
     }
 }
